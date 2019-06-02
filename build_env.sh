e181beb9da323d5b7cc92d27a29a55a941355654
huangwei <1845739048@qq.com>
2019-06-02 14:16:28 +0800

    add build_env

---
+++ build_env.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# User define:
+#export PROJECT_ROOT=/home/hunagwei/study/computer-graphics/my_computer_graphics
+#export REDBOOK_BUILD_DIR=$PORJECT_ROOT/../build_redbook
+
+export REDBOOK_EXAMPLES_DIR=$PROJECT_ROOT/redbook_example
+export REDBOOK_INCLUDE_DIR=$REDBOOK_EXAMPLES_DIR/include
+export REDBOOK_LIB_PATH=$REDBOOK_BUILD_DIR/lib
+
